b842f5f670ada4ca3185ef7d930fcab309273223
Wheatley <slendermanwithaface@gmail.com>
2016-12-07 19:12:28 +0000

    Transitioned player handling code to server

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ var debug = process.env.NODE_ENV !== 'production';
 var webpack = require('webpack');
 
 module.exports = {
-    context: __dirname+'/src/client',
+    context: __dirname,
     devtool: debug ? 'inline-sourcemap' : null,
-    entry: './js/app.js',
+    entry: './src/client/js/app.js',
     output: {
         path: __dirname + '/src/client/js',
         filename: 'app.min.js'
